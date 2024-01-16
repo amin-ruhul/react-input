13e24de1c5fe56fd37fec8436428b670b54e294a
Ruhul <ruhulinfo50@gmail.com>
2024-01-16 16:23:59 +0600

    Create user data

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>gg</div>;
+}
+
+export default App;
---
+++ src/data/users.ts
@@ -0,0 +1,40 @@
+export const users = [
+  {
+    id: "1",
+    name: "Calvin Richards",
+    email: "calvin.richards@example.com",
+    pic: "https://randomuser.me/api/portraits/men/6.jpg",
+  },
+  {
+    id: "2",
+    name: "Terri Olson",
+    email: "terri.olson@example.com",
+    pic: "https://randomuser.me/api/portraits/women/43.jpg",
+  },
+  {
+    id: "3",
+    name: "Elsie Ortiz",
+    email: "elsie.ortiz@example.com",
+    pic: "https://randomuser.me/api/portraits/women/72.jpg",
+  },
+  {
+    id: "4",
+    name: "Shawn Kuhn",
+    email: "shawn.kuhn@example.com",
+    pic: "https://randomuser.me/api/portraits/men/38.jpg",
+  },
+
+  {
+    id: "5",
+    name: "Avery Fisher",
+    email: "avery.fisher@example.com",
+    pic: "https://randomuser.me/api/portraits/women/1.jpg",
+  },
+
+  {
+    id: "6",
+    name: "Brian Coleman",
+    email: "brian.coleman@example.com",
+    pic: "https://randomuser.me/api/portraits/men/56.jpg",
+  },
+];
